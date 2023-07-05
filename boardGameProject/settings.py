4ad7f05154896ec391dcf0be7401ab7d8dc9a0ab
kohei <koheimtri3@gmail.com>
2023-07-06 01:32:29 +0900

    add ALLOWED HOSTS new EC2 instance IP

--- boardGameProject/settings.py
+++ boardGameProject/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = [
     "https://board-game-studio.vercel.app",
     "BoardGameStudio.ap-northeast-1.elasticbeanstalk.com",
     '172.31.0.254',  # EC2のIPアドレス
+    '172.31.4.164',  # EC2のIPアドレス
     'board-game-studio.net',
 ]
 
