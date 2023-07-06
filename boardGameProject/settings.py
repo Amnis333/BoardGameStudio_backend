8631f87f73e01084b72b4d21979dd3b4d70e6b9e
kohei <koheimtri3@gmail.com>
2023-07-06 10:33:04 +0900

    SECRETKEYの設定元に戻す

--- boardGameProject/settings.py
+++ boardGameProject/settings.py
@@ -14,10 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if "DJANGO_SECRET_KEY" in os.environ:
-    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-else:
-    raise Exception("The DJANGO_SECRET_KEY environment variable is not set!")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
