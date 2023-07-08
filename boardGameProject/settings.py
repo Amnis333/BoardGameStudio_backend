e7106183e0fc3d78840e33dfc6290ea57904993b
kohei <koheimtri3@gmail.com>
2023-07-08 17:28:03 +0900

    add RDS_DB_NAME

--- boardGameProject/settings.py
+++ boardGameProject/settings.py
@@ -84,6 +84,7 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'mysql.connector.django',
+            'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
